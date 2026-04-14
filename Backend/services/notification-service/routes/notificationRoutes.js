4c470d6638b50bcdbaf4608052e305865b640cbe
Rachitha Eshan <rachithaeshan@gmail.com>
2026-04-15 00:35:58 +0530

    Fixed: Implemented backend of Notification service 🔔

---
+++ Backend/services/notification-service/routes/notificationRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    appointmentBooked,
+    consultationCompleted,
+    appointmentCancelled,
+    getAllNotifications,
+    getNotificationsByEmail
+} from "../controllers/notificationController.js";
+
+const router = express.Router();
+
+router.post("/appointment-booked", appointmentBooked);
+router.post("/consultation-completed", consultationCompleted);
+router.post("/appointment-cancelled", appointmentCancelled);
+router.get("/all", getAllNotifications);
+router.get("/email/:email", getNotificationsByEmail);
+
+export default router;
